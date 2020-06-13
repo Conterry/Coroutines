02d34c22f9c0189468dbb8aba02de0d57e61b2ef
Conterry <glebois05@gmail.com>
2020-06-13 12:58:49 +0300

    ...

---
+++ Assets/GGCOMP.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GGCOMP : MonoBehaviour {
+
+
+
+
+	public int param;
+	private int CountNow;
+	private const int BIRST	= 50;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		int maxNumber = CountNow + BIRST;
+		if (maxNumber > param)
+		{
+			maxNumber = param;
+		}
+
+		while (CountNow < maxNumber)
+		{
+			print(CountNow);
+			CountNow++;
+		}
+
+	}
+}
---
+++ Assets/NormalSpeed.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalSpeed : MonoBehaviour {
+
+	public int param;
+	private const int BIRST = 50;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(PrintNumbers(param));
+	}
+
+	private IEnumerator TRTR()
+	{
+		yield return new WaitForSeconds(5);
+		yield return PrintNumbers(param);
+	}
+
+
+
+
+
+	private IEnumerator PrintNumbers(int AmountNumbersToPrint)
+	{
+		int CountNow = 0;
+		while (CountNow < AmountNumbersToPrint)
+		{
+
+			int maxNumber = CountNow + BIRST;
+			if (maxNumber > AmountNumbersToPrint)
+			{
+				maxNumber = AmountNumbersToPrint;
+			}
+
+			while (CountNow < maxNumber)
+			{
+				print(CountNow);
+				CountNow++;
+			}
+
+			yield return null;
+
+		}
+	}  
+
+}
